f8732920999a15009a70af3627f2b56995417ae9
Fabio Ramos <fabioramos10h@gmail.com>
2021-10-14 10:41:23 +0100

    done 1

--- src/layouts/home/Navbar.vue
+++ src/layouts/home/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
- <!--<div>
+  <!--<div>
         <v-app-bar 
           app 
           elevation="6"
@@ -35,104 +35,96 @@
         
         </v-app-bar>
     </div>-->
-     <nav >
-      <v-app-bar app clipped-left >
-        <v-app-bar-nav-icon  @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title class="text-uppercase grey--text">
-           <router-link to="/">
-              <v-img max-width="230px" height="100%" src="/EFLogo.png" > </v-img>
-            </router-link>
-        </v-toolbar-title>
-        
-      </v-app-bar>
+  <nav>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <router-link to="/">
+          <v-img max-width="230px" height="100%" src="/EFLogo.png"> </v-img>
+        </router-link>
+      </v-toolbar-title>
+    </v-app-bar>
 
-      <!--expand-on-hover-->
-      <v-navigation-drawer
-        width="270"
-        app
-        src="/imgnavbar.jpg"
-        gradient="to right, rgba(14,12,11,.71), rgba(14,12,11,.71)"
-        expand-on-hove
-        clipped
-        v-model="drawer"
-        class=" white--text"
-      >
+    <!--expand-on-hover-->
+    <v-navigation-drawer
+      width="270"
+      app
+      src="/imgnavbar.jpg"
+      gradient="to right, rgba(14,12,11,.71), rgba(14,12,11,.71)"
+      expand-on-hove
+      clipped
+      v-model="drawer"
+      class="white--text"
+    >
+      <v-list nav>
+        <!---INICIO-->
+        <v-list-item-group color="primary">
+          <v-list-item :to="{ path: '/', name: '' }">
+            <v-list-item-icon>
+              <v-icon>home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Início</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+        <!---->
 
-        <v-list nav >
-          <!---INICIO-->
-          <v-list-item-group
-            color="primary"
-          >
-          
-              <v-list-item :to="{path: '/', name: ''}">
-                <v-list-item-icon>
-                  <v-icon>home</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title>Início</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-          </v-list-item-group>
-          <!---->
+        <!---ADICIONAR ANUNCIO-->
+        <v-list-item-group color="primary">
+          <v-list-item :to="{ path: '/sobre', name: 'Sobre' }">
+            <v-list-item-icon>
+              <v-icon>spa</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Sobre</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+        <!---->
 
-          <!---ADICIONAR ANUNCIO-->
-          <v-list-item-group
-            color="primary"
-          >
-          
-              <v-list-item :to="{path: '/sobre', name: 'Sobre'}">
-                <v-list-item-icon>
-                  <v-icon>spa</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title>Sobre</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-          </v-list-item-group>
-          <!---->
+        <!---ADICIONAR REABILITAÇÃO FÍSICA-->
+        <v-list-item-group color="primary">
+          <v-list-item :to="{ path: '/Services', name: 'Serviços' }">
+            <v-list-item-icon>
+              <v-icon>fitness_center</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Serviços</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+        <!---->
 
-          <!---ADICIONAR REABILITAÇÃO FÍSICA-->
-          <v-list-item-group
-            color="primary"
-          >
-          
-              <v-list-item :to="{path: '/Services', name: 'Serviços'}">
-                <v-list-item-icon>
-                  <v-icon>fitness_center</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title>Serviços</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-          </v-list-item-group>
-          <!---->
-
-        </v-list>
-      
-      </v-navigation-drawer>
-    </nav>
+        <v-list-item-group color="primary" v-show="!mobile">
+          <v-list-item :to="{ path: '/sobre', name: 'Sobre' }">
+            <v-list-item-icon>
+              <v-icon>spa</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Sobre</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
-
 export default {
+  data() {
+    return {
+      drawer: false,
 
-    data() {
-        return {
-
-          
-            drawer:false,
-
-            tabs: null,
-             items: [
-             'Início',
-              'Sobre',
-              'Serviços',
-            ],
-        }
-    },
-  }
+      tabs: null,
+      items: ["Início", "Sobre", "Serviços"],
+    };
+  },
+};
 </script>
 <style lang="css">
-  a { text-decoration: none; }
+a {
+  text-decoration: none;
+}
 </style>
